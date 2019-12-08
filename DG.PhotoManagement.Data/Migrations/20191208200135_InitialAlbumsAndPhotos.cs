feebea85d19f533a352fbacb36e146cac3fbb368
dgtechboost <25640061+dgtechboost@users.noreply.github.com>
2019-12-08 20:07:59 +0000

    initial migration

---
+++ DG.PhotoManagement.Data/Migrations/20191208200135_InitialAlbumsAndPhotos.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DG.PhotoManagement.Data.Migrations
+{
+    public partial class InitialAlbumsAndPhotos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Albums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Albums", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<short>(type: "SMALLINT", nullable: false),
+                    AlbumId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(maxLength: 2000, nullable: true),
+                    Url = table.Column<string>(nullable: true),
+                    ThumbnailUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Photos_Albums_AlbumId",
+                        column: x => x.AlbumId,
+                        principalTable: "Albums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Photos_AlbumId",
+                table: "Photos",
+                column: "AlbumId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+
+            migrationBuilder.DropTable(
+                name: "Albums");
+        }
+    }
+}
